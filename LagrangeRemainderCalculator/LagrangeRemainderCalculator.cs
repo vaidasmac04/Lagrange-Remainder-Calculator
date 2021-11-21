f9f959e2db88a11817916ab1d2171b4eb2065d53
Vaidas <vaidasmac04@gmail.com>
2021-11-21 12:01:08 +0200

    add console ui for selecting interval and x value

--- LagrangeRemainderCalculator/LagrangeRemainderCalculator.cs
+++ LagrangeRemainderCalculator/LagrangeRemainderCalculator.cs
@@ -25,32 +25,31 @@ namespace LagrangeRemainderCalculator
 
             if(x < interval.Start || x > interval.End)
             {
-                throw new ArgumentException($"Wrong interval provided: point x = '{x}' " +
+                throw new ArgumentException($"Wrong point provided: x = '{x}' " +
                    $"must be in [{interval.Start};{interval.End}]");
             }
 
+            Interval newInterval = null;
             if (x < _x0)
             {
-                interval.Start = x;
-                interval.End = 0;
+                newInterval = new Interval(x, 0);
             } 
             else if (x > _x0)
             {
-                interval.Start = 0;
-                interval.End = x;
+                newInterval = new Interval(0, x);
             }
             else
             {
                 return 0;
             }
 
-            var intervalLength = Math.Abs(interval.End - interval.Start);
+            var intervalLength = Math.Abs(newInterval.End - newInterval.Start);
             var stepSize = intervalLength / stepCount;
 
-            decimal currentIntervalPoint = x < _x0 ? interval.Start : interval.Start + stepSize;
+            decimal currentIntervalPoint = x < _x0 ? newInterval.Start : newInterval.Start + stepSize;
 
             var results = new List<decimal>();
-            var end = x < _x0 ? interval.End : interval.End + stepSize;
+            var end = x < _x0 ? newInterval.End : newInterval.End + stepSize;
             while (currentIntervalPoint < end)
             {
                 var remainderValue = CalculateRemainder(function, currentIntervalPoint, x);
--- LagrangeRemainderCalculator/Program.cs
+++ LagrangeRemainderCalculator/Program.cs
@@ -1,6 +1,4 @@
-﻿using MathNet.Symbolics;
-using System;
-using System.Configuration;
+﻿using System;
 using System.IO;
 using Expr = MathNet.Symbolics.SymbolicExpression;
 
@@ -8,7 +6,10 @@ namespace LagrangeRemainderCalculator
 {
     class Program
     {
-        static void Main()
+        private static Interval _interval = new Interval(-0.5m, 0.5m);
+        private static decimal _x = 0.4m;
+
+        static int Main()
         {
             Expr function = null;
 
@@ -27,24 +28,116 @@ namespace LagrangeRemainderCalculator
 
             if (function != null)
             {
-                var interval = new Interval(-0.5m, 0.5m);
                 ILagrangeRemainderCalculator lagrangeRemainderCalculator =
                     new LagrangeRemainderCalculator();
 
-                var remainder = lagrangeRemainderCalculator.CalculateRemainder(
-                    function, 
-                    interval, 
-                    0.6m,
-                    10);
+            ShowMainMenuLabel:
+                Console.WriteLine("***********************************************************");
+                Console.WriteLine($"Current selection: interval - {_interval}, x = {_x}");
+                Console.WriteLine("***********************************************************");
 
-                Console.WriteLine($"Remainder: {remainder}");
+                Console.WriteLine("1. Change interval");
+                Console.WriteLine("2. Change x");
+                Console.WriteLine("3. Calculate remainder");
+                Console.WriteLine("4. Close program");
+
+            HandleMainMenuLabel:
+                try
+                {
+                    Console.Write("Your input: ");
+                    int choice = int.Parse(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Choose interval: ");
+                            Console.WriteLine("1. [-0,5; 0,5]");
+                            Console.WriteLine("2. [-0,1; 0,1]");
+                            Console.WriteLine("3. [-0,001; 0,001]");
+
+                        IntervalSelectionLabel:
+                            Console.Write("Your input: ");
+                            try
+                            {
+                                int intervalChoice = int.Parse(Console.ReadLine());
+
+                                switch (intervalChoice)
+                                {
+                                    case 1:
+                                        _interval = new Interval(-0.5m, 0.5m);
+                                        break;
+                                    case 2:
+                                        _interval = new Interval(-0.1m, 0.1m);
+                                        break;
+                                    case 3:
+                                        _interval = new Interval(-0.001m, 0.001m);
+                                        break;
+                                    default:
+                                        PrintCalculatorMessage("interval selection failed. Try again.");
+                                        goto IntervalSelectionLabel;
+                                }
+
+                                PrintCalculatorMessage("interval selected successfuly");
+                            }
+                            catch (FormatException)
+                            {
+                                PrintCalculatorMessage("interval selection failed. Try again.");
+                                goto IntervalSelectionLabel;
+                            }
+                            break;
+                        case 2:
+                            Console.Write("Your input: ");
+
+                            HandleXChoice:
+                                try
+                                {
+                                    _x = decimal.Parse(Console.ReadLine());
+                                }
+                                catch (FormatException)
+                                {
+                                    PrintCalculatorMessage("x value selection failed. Try again.");
+                                    goto HandleXChoice;
+                                }
+
+                            PrintCalculatorMessage("x value selected successfuly");
+
+                            break;
+                        case 3:
+                            var remainder = lagrangeRemainderCalculator.CalculateRemainder(
+                               function,
+                               _interval,
+                               _x,
+                               10);
+                            PrintCalculatorMessage($"remainder calculated successfuly: {remainder}");
+                            break;
+                        case 4:
+                            return 0;
+                        default:
+                            PrintCalculatorMessage("action selection failed. Try again.");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    PrintCalculatorMessage("action selection failed. Try again.");
+                    goto HandleMainMenuLabel;
+                }
+                catch (ArgumentException argumentException) 
+                {
+                    PrintCalculatorMessage(argumentException.Message);
+                    goto ShowMainMenuLabel;
+                }
+
+                Console.WriteLine();
+                goto ShowMainMenuLabel;
             }
+
+            return 0;
         }
 
         private static Expr ParseFunction(string filePath)
         {
             var functionAsString = File.ReadAllText(filePath);
-            Console.WriteLine(functionAsString);
 
             if (string.IsNullOrEmpty(functionAsString))
             {
@@ -52,7 +145,11 @@ namespace LagrangeRemainderCalculator
             }
 
             return Expr.Parse(functionAsString);
-          
+        }
+
+        private static void PrintCalculatorMessage(string message)
+        {
+            Console.WriteLine($"--> Calculator: {message}");
         }
     }
 }
