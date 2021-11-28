ae74ea44b62d1aa19bb3eec05dd76e660a4bdaf7
vaidasmac04 <51237931+vaidasmac04@users.noreply.github.com>
2021-11-28 14:49:21 +0200

    add remainder evaluator in interval (#6)

---
+++ LagrangeRemainder/Models/EvaluationInterval.cs
@@ -0,0 +1,21 @@
+﻿namespace LagrangeRemainder.Models
+{
+    public class EvaluationInterval : Interval
+    {
+        private decimal _maxDerivativeValueX;
+
+        public decimal MaxDerivativeValueX 
+        { 
+            get => _maxDerivativeValueX; 
+            set => _maxDerivativeValueX = value; 
+        }
+
+        public EvaluationInterval(
+            decimal start, 
+            decimal end, 
+            decimal maxDerivativeValueX) : base(start, end)
+        {
+            _maxDerivativeValueX = maxDerivativeValueX;
+        }
+    }
+}
---
+++ LagrangeRemainderEvaluatorUI/Program.cs
@@ -0,0 +1,80 @@
+﻿using LagrangeRemainder;
+using LagrangeRemainder.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Expr = MathNet.Symbolics.SymbolicExpression;
+
+namespace LagrangeRemainderEvaluatorUI
+{
+    class Program
+    {
+        private static decimal _x = 0.4m;
+
+        static int Main()
+        {
+            Expr function = null;
+            Expr taylor = null;
+
+            try
+            {
+                var (Function, Taylor) = ParseFunctions("functions.json");
+                function = Function;
+                taylor = Taylor;
+            }
+            catch (IOException ioException)
+            {
+                Console.WriteLine(ioException.Message);
+            }
+            catch (ArgumentException argumentException)
+            {
+                Console.WriteLine(argumentException.Message);
+            }
+
+            if (function != null && taylor != null)
+            {
+                ILagrangeRemainderEvaluator lagrangeRemainderEvaluator =
+                    new LagrangeRemainderCalculator();
+
+                var evaluationIntervals = new List<EvaluationInterval>
+                {
+                    new EvaluationInterval(-0.5m, 0.5m, -0.5m),
+                    new EvaluationInterval(-0.1m, 0.1m, -0.1m),
+                    new EvaluationInterval(-0.001m, 0.001m, -0.001m)
+                };
+
+                var evaluationResults = lagrangeRemainderEvaluator
+                    .EvaluateRemainder(function, evaluationIntervals);
+
+                foreach(var evaluationResult in evaluationResults)
+                {
+                    Console.WriteLine($"Intervalas: {evaluationResult.Interval}, " +
+                        $"Lagranžo liekamasis narys: {evaluationResult.Remainder}");
+                }
+            }
+
+            return 0;
+        }
+
+        private static (Expr Function, Expr Taylor) ParseFunctions(string filePath)
+        {
+            var functionsAsJson = JObject.Parse(File.ReadAllText(filePath));
+
+            var function = functionsAsJson["function"].ToString();
+            var taylor = functionsAsJson["taylor"].ToString();
+
+            if (string.IsNullOrEmpty(function))
+            {
+                throw new ArgumentException("Klaida. Funkcija nepateikta.");
+            }
+
+            if (string.IsNullOrEmpty(taylor))
+            {
+                throw new ArgumentException("Klaida. Teiloro eilutė nepateikta.");
+            }
+
+            return (Expr.Parse(function), Expr.Parse(taylor));
+        }
+    }
+}
